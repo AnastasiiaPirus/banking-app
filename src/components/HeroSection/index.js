f6c8b0f0e6063543a1824a97526d8d3533d21114
AnastasiiaPirus <a.pirus2000@gmail.com>
2023-01-13 17:44:54 -0500

    Added Footer and Sign In.

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -24,7 +24,7 @@ const HeroSection = () => {
 
 
   return (
-    <HeroContainer>
+    <HeroContainer id = "home">
         <HeroBg>
             <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
         </HeroBg>
@@ -32,7 +32,7 @@ const HeroSection = () => {
             <HeroH1>Virtual Banking Made Easy</HeroH1>
             <HeroP>Sign up for new account today and receive $250 in credit towards your next payment.</HeroP>
             <HeroBtnWrapper>
-              <Button to="signup" onMouseEnter = {onHover} onMouseLeave = {onHover} primary = "true" dark="true">Get started {hover ? <ArrowForward /> : <ArrowRight />} </Button>
+              <Button to="signup" onMouseEnter = {onHover} onMouseLeave = {onHover} primary = "true" dark="true" smooth={true} duration={500} spy={true} exact = "true" offset={-80}>Get started {hover ? <ArrowForward /> : <ArrowRight />} </Button>
             </HeroBtnWrapper>
         </HeroContent>
     </HeroContainer>
